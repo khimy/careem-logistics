294fa13668461c678dba22519aa35ca3896966e9
Vivek <vivek0811141@gmail.com>
2017-02-26 09:23:25 +0530

    application

---
+++ ca-order/ca-order-app/src/main/java/com/ca/order/api/NotificationApi.java
@@ -0,0 +1,15 @@
+package com.ca.order.api;
+
+import java.util.Date;
+
+/**
+ * Created by Vivek on 26-02-2017.
+ */
+public class NotificationApi {
+
+    public Long orderId;
+    public Date startTime;
+    public Date endTime;
+    public String currentProcess;
+
+}
